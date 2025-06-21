f3c8d37e3fadde385420370994393414bd66e120
Alibek0301 <alibek-u@mail.ru>
2025-06-21 18:54:43 +0500

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const rateMap = {};
 const LIMIT = 3;
